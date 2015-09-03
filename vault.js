7dad2fb5b4a3c757778236319664ce5a048724a7
Bryan Alexander <bryanalexander@me.com>
2015-09-03 10:48:20 -1000

    completed vault exercise, all tests passing

--- vault.js
+++ vault.js
@@ -1,4 +1,33 @@
 'use strict';
+
 module.exports = function() {
-  
+
+  var secret = {};
+
+  return {
+
+    setValue : function( key, value ) {
+
+      secret[key] = value;
+
+    },
+
+    getValue : function( key ) {
+
+      if (secret.hasOwnProperty(key)) {
+
+        return secret[key];
+
+      } else {
+
+        return null;
+
+      }
+
+    }
+
+  };
+
 };
+
+
